5805f372f30c816bbce6925757f7c342d4273e08
Damian Domzalski <damiandomzalskipl@gmail.com>
2018-01-26 23:43:44 +0100

    refactoring CommentModel validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :movie
-  validates :user_id, presence: true, uniqueness: { scope: :movie_id, message: "You can add only one comment per movie, delete the previous one if you want to add a new one" }
+  validates_presence_of :user_id, :movie_id, :content
+  validates_uniqueness_of :user_id, :message =>"You can add only one comment per movie, delete the previous one if you want to add a new one"
+
 
   def self.top_commenters
     Comment.select("user_id, count(user_id) as count").group(:user_id).where("created_at >= ?", 1.week.ago.utc).order("count DESC").limit(10)
