5804c089840207c092ed6ac304c9eab072834fca
Damian Domzalski <damiandomzalskipl@gmail.com>
2018-01-26 23:47:42 +0100

    Working CommentModel unique validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+  validates :user_id, presence: true, uniqueness: { scope: :movie_id, message: "You can add only one comment per movie, delete the previous one if you want to add a new one" }
   validates_presence_of :user_id, :movie_id, :content
-  validates_uniqueness_of :user_id, :message =>"You can add only one comment per movie, delete the previous one if you want to add a new one"
 
 
   def self.top_commenters
